06474d2674750aaaa52cd1cc96ddafdcd2acdaa0
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-24 21:22:32 +0200

    Checks if the node is a root

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,7 @@ void binary_tree_delete(binary_tree_t *tree);
 
 /*checks of a node is a leaf*/
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/*checks if a given node is a root*/
+int binary_tree_is_root(const binary_tree_t *node);
 #endif
